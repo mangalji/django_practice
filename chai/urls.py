8e61029011e136fbc8e18c7c55da8741f13ac82f
mangalji <rajmangal03@gmail.com>
2025-12-15 00:31:32 +0530

    updated urls

--- chai/urls.py
+++ chai/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home, name='home'),
-    path('about/',views.about,name='about'),
-    path('contact/',views.contact,name='contact'),
-    path('profile/',views.profile,name='profile'),
+    path('',views.all_chai, name='all_chai'),
 ]
