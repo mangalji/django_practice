5af7cf64ad79785cffa8c72e38a1e9ae08f0a41c
mangalji <rajmangal03@gmail.com>
2025-12-15 00:37:53 +0530

    code updated

--- chai/urls.py
+++ chai/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+# now if anyone use localhost:8000/chai he saw the all_chai.html page
 urlpatterns = [
     path('',views.all_chai, name='all_chai'),
 ]
